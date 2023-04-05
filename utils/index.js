321ca75a93b61915ed83b0138801620269a33898
Djordje Arsenovic <dj.arsenovic@gmail.com>
2023-04-05 11:42:30 +0200

    Final application version

---
+++ utils/index.js
@@ -0,0 +1,1 @@
+export const checkImageURL = (url) => {  if (!url) return false;  else {    const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');    return pattern.test(url);  }};
